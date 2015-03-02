2a7bb37f546dc624ee322f1c04d093b8fddbfc50
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2015-03-02 20:32:50 +0000

    Initial commit.

---
+++ src/main/java/uk/co/probablyfine/dirty/DirtyDB.java
@@ -0,0 +1,5 @@
+package uk.co.probablyfine.dirty;
+
+public class DirtyDB {
+
+}
---
+++ src/test/java/uk/co/probablyfine/dirty/DirtyDBTest.java
@@ -0,0 +1,12 @@
+package uk.co.probablyfine.dirty;
+
+import org.junit.Test;
+
+public class DirtyDBTest {
+
+  @Test
+  public void shouldCreateDB() {
+    new DirtyDB();
+  }
+
+}
