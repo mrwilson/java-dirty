7b32289bbe2c5f56b5021db85b0b20ec718527a2
Gunnar Ahlberg <gunnar@gunnarahlberg.com>
2015-04-17 21:40:54 +0200

    Cleanup of 'reset' for clarity and performance. Reset can clear the list of elements in one go instead of remove one by one. This way the internal ArrayList is not copied over and over for each removal. The performance should be measurable

--- src/main/java/uk/co/probablyfine/dirty/Store.java
+++ src/main/java/uk/co/probablyfine/dirty/Store.java
@@ -129,10 +129,10 @@ public class Store<T> {
 
   public void reset() {
     this.size = 0;
-    for (int i = 1; i < partitions.size(); i++) {
-      partitions.remove(i);
-    }
-    this.partitions.get(0).putInt(0, 0);
+    MappedByteBuffer head = partitions.get(0);
+    partitions.clear();
+    partitions.add(head);
+    head.putInt(0, 0);
   }
 
   public interface WithFile<T> {
